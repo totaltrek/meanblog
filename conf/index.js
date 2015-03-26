8b2e5063891753d214db918689614f5eef3239dc
totaltrek <github@thetotaltrek.com>
2015-03-26 03:58:21 +0000

    not really sure, just want to catch up on the laptop

--- conf/index.js
+++ conf/index.js
@@ -1,15 +1,56 @@
-var fs = require( 'fs' ),
-	path = require( 'path' ),
-	lib = require( '../lib' ),
-	files = {
-		all: fs.readdirSync( __dirname ),
-		defaults: [],
-		locals: [],
-		others: []
-	};
-var tooOld;
-if ( tooOld ) {
-	require( 'grunt' ).tasks( [ 'rebuildConfig' ] );
+function rebuild ( request, response, next ) {
+	require( 'grunt' ).tasks( [ 'rebuildConfig' ], {}, oncomplete );
 }
 
-module.exports = config;
+function oncomplete ( request, response, next ) {
+	return function ( appconfig ) {
+		if ( appconfig && appconfig != {} ) {
+			request.app.set( 'config', appconfig );
+			request.emit( 'config:rebuild', appconfig, request, response, next );
+			response.emit( 'config:rebuild', appconfig, request, response, next );
+		} else {
+			next();
+		}
+	};
+}
+
+function updateConfigs( request, response, next ) {
+	var config = request.app.get( 'config' );
+	var nodes = Object.keys( config );
+	nodes.forEach( function ( node ) {
+		request.emit( node + ':updateConfig', config[ node ], request, response, next );
+		response.emit( node + ':updateConfig', config[ node ], request, response, next );
+	});
+}
+
+function middleware ( request, response, next ) {
+	request.on( 'config:update', rebuild );
+	response.on( 'config:update', rebuild );
+	request.on( 'config:rebuild', updateConfigs );
+	response.on( 'config:rebuild', updateConfigs );
+}
+
+module.exports = {
+	express: middleware,
+	hapi: middleware
+};
+
+/* implement like so
+subscriber:
+var config = {};
+function updateMyConfig ( newconfig, request, response, next ) {
+	config = newconfig;
+	if ( next ) {
+		next();
+	}
+}
+function someMiddleware ( request, response, next ) {
+	request.on( 'testing:updateConfig', updateMyConfig );
+}
+publisher:
+function announceUpdate ( request, response, next ) {
+	
+	request.emit( 'config:update', request, response, next );
+}
+function 
+*/
--- index.js
+++ index.js
@@ -1,15 +1,71 @@
-var express = require( 'express' );
-var config = require( './conf' );
+/* requires */
+/* logging first */
+var log = require( './log' );
+// third party
+var express = require( 'express' ); // http wiring framework
+var Session = require( 'express-session' ); // session plugin for express
+// published
+var Velvetrope = require( 'velvetrope' ); // vulnerability scanner
+var Charmed = require( 'charmed' ); // character escapes conversion
+// local
+var Conf = require( './conf' ); // configurator
+var Lib = require( './lib' ); // common processes - keep very small
+var Login = require( './login' ); // logged-out interface
+var Models = require( './models' ); // data models 
+var Orm = require( './orm-adapter' ); // orm event and request hooks
+var Controllers = require( './controllers' ); // event hooks for logic
+var Services = require( './services' ); // async services
+var Gateway = require( './gateway' ); // router
+
+/* app inits */
 var app = express();
+app.set( 'lib', Lib );
+var config = Conf( app );
+app.set( 'config', config );
 
-app.set( config )
+/* the point is not to use express too much, just enough
+app.set( config.app );
+// app.set( lib ); // what does this actually do?
 app.locals.config = config;
+app.locals.lib = lib;
+*/
 
-var Session = require( 'express-session' );
+/* class inits */
+// var lib = Lib.init( config.lib ); // lib doesn't need configuration
+var velvetrope = Velvetrope.init( config.velvetrope );
 var session = Session( config.session );
-app.use( session );
+var charmed = Charmed.init( config.charmed );
+var login = Login.init( config.login );
+var models = Models.init( config.model );
+var orm = Orm.init( config.orm );
+var services = Services.init( config.services );
+var controllers = Controllers.init( config.controllers );
+var gateway = Gateway.init( app );
 
+/* middlewares */
+/* in order of execution */
+
+/* safe ( no http ) */
+app.use( charmed.express );
+/* security */
+app.use( velvetrope.express );
+/* use third party anywhere below security, not above */ 
+app.use( session );
+/* login short-circuit */
+app.use( login.express );
+/* models */
+app.use( models.express );
+/* orm adapter */
+app.use( orm.express );
+/* controllers */
+app.use( controllers.express );
+/* services */
+app.use( services.express );
+/* routes */
+app.use( gateway.express );
+
+/* server */
 app.listen( config.server.port, config.server.host, listening );
 function listening ( req, res, next ) {
-    console.log( 'listening', config.server );
+    gateway.emit( 'listening' );
 }
